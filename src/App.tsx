df17d2233a86da4febc4b3447adfc225987c13a0
Kristinn Roach <112353472+KristinnRoach@users.noreply.github.com>
2026-09-08 23:22:38 +0000

    Add AM modulation octave offset control (#19)
    Proper cleanup and styling deferred to follow up slice

--- src/App.tsx
+++ src/App.tsx
@@ -630,9 +630,21 @@ const App: Component = () => {
                 style="display: inline-flex; flex-direction: column; align-items: center; gap: 2px;"
               >
                 <ParamKnob param="amMod" label="AM" player={samplePlayer()} />
-                <ModulationWaveformSelect value={amWaveform()} onChange={setAmWaveform} />
+                <span style="display: flex; flex-direction: row; align-items: space-between; gap: 4px;">
+                  <ModulationWaveformSelect value={amWaveform()} onChange={setAmWaveform} />
+                  <input
+                    style="text-align: center;"
+                    type="number"
+                    inputmode="numeric"
+                    pattern="[0-9]*"
+                    min="-4"
+                    max="3"
+                    value="1"
+                    on:change={(e) => samplePlayer()?.setAMModOctaveOffset(Number(e.target.value))}
+                  />
+                </span>
               </div>
-              {/* TODO: uncomment to test pre-release param <ParamKnob param="amModOctaveOffset" player={samplePlayer()} /> */}
+              {/* <ParamKnob param="amModOctaveOffset" player={samplePlayer()} /> */}
             </div>
           </fieldset>
 
