c760200ad3dc7affd130e9a191733718d9a67552
kiddi <kristinnroach@gmail.com>
2026-09-10 23:03:39 +0000

    formatting and nowrap on env controls top-bar

--- src/components/envelopes/EnvelopeControls.tsx
+++ src/components/envelopes/EnvelopeControls.tsx
@@ -30,100 +30,100 @@ export const EnvelopeControls: Component<EnvelopeControlsProps> = (props) => (
     </select>
 
     <div class={styles.toggles}>
-        <input
-          use:tooltip={['Enabled']}
-          type="checkbox"
-          checked={props.state?.enabled ?? false}
-          disabled={!props.state}
-          onChange={(event) =>
-            props.onUpdate((current) => ({ ...current, enabled: event.currentTarget.checked }))
-          }
-        />
+      <input
+        use:tooltip={['Enabled']}
+        type="checkbox"
+        checked={props.state?.enabled ?? false}
+        disabled={!props.state}
+        onChange={(event) =>
+          props.onUpdate((current) => ({ ...current, enabled: event.currentTarget.checked }))
+        }
+      />
 
-        <input
-          use:tooltip={['Loop']}
-          type="checkbox"
-          checked={props.state?.loop ?? false}
-          disabled={!props.state}
-          onChange={(event) =>
-            props.onUpdate((current) => ({ ...current, loop: event.currentTarget.checked }))
-          }
-        />
+      <input
+        use:tooltip={['Loop']}
+        type="checkbox"
+        checked={props.state?.loop ?? false}
+        disabled={!props.state}
+        onChange={(event) =>
+          props.onUpdate((current) => ({ ...current, loop: event.currentTarget.checked }))
+        }
+      />
 
-        <input
-          use:tooltip={['Rate sync']}
-          type="checkbox"
-          checked={props.state?.playbackRateSync ?? false}
-          disabled={!props.state}
-          onChange={(event) =>
-            props.onUpdate((current) => ({
-              ...current,
-              playbackRateSync: event.currentTarget.checked,
-            }))
-          }
-        />
+      <input
+        use:tooltip={['Rate sync']}
+        type="checkbox"
+        checked={props.state?.playbackRateSync ?? false}
+        disabled={!props.state}
+        onChange={(event) =>
+          props.onUpdate((current) => ({
+            ...current,
+            playbackRateSync: event.currentTarget.checked,
+          }))
+        }
+      />
     </div>
 
     <Show when={props.state?.shape.kind === 'points' ? props.state.shape : null}>
       {(shape) => (
         <div class={styles.pointRoleSelectors}>
-            <select
-              use:tooltip={['Select Sustain Point']}
-              value={String(shape().sustainIndex ?? 'none')}
-              onChange={(event) =>
-                props.onUpdate((current) =>
-                  current.shape.kind === 'points'
-                    ? {
-                        ...current,
-                        shape: {
-                          ...current.shape,
-                          sustainIndex:
-                            event.currentTarget.value === 'none'
-                              ? null
-                              : Number(event.currentTarget.value),
-                        },
-                      }
-                    : current,
-                )
-              }
-            >
-              <option value="none" selected={shape().sustainIndex == null}>
-                off
-              </option>
-              <For each={shape().points}>
-                {(_point, index) => (
-                  <option value={String(index())} selected={index() === shape().sustainIndex}>
-                    {index()}
-                  </option>
-                )}
-              </For>
-            </select>
+          <select
+            use:tooltip={['Select Sustain Point']}
+            value={String(shape().sustainIndex ?? 'none')}
+            onChange={(event) =>
+              props.onUpdate((current) =>
+                current.shape.kind === 'points'
+                  ? {
+                      ...current,
+                      shape: {
+                        ...current.shape,
+                        sustainIndex:
+                          event.currentTarget.value === 'none'
+                            ? null
+                            : Number(event.currentTarget.value),
+                      },
+                    }
+                  : current,
+              )
+            }
+          >
+            <option value="none" selected={shape().sustainIndex == null}>
+              off
+            </option>
+            <For each={shape().points}>
+              {(_point, index) => (
+                <option value={String(index())} selected={index() === shape().sustainIndex}>
+                  {index()}
+                </option>
+              )}
+            </For>
+          </select>
 
-            <select
-              use:tooltip={['Select Release Point']}
-              value={String(shape().releaseIndex)}
-              onChange={(event) =>
-                props.onUpdate((current) =>
-                  current.shape.kind === 'points'
-                    ? {
-                        ...current,
-                        shape: {
-                          ...current.shape,
-                          releaseIndex: Number(event.currentTarget.value),
-                        },
-                      }
-                    : current,
-                )
-              }
-            >
-              <For each={shape().points}>
-                {(_point, index) => (
-                  <option value={String(index())} selected={index() === shape().releaseIndex}>
-                    {index()}
-                  </option>
-                )}
-              </For>
-            </select>
+          <select
+            use:tooltip={['Select Release Point']}
+            value={String(shape().releaseIndex)}
+            onChange={(event) =>
+              props.onUpdate((current) =>
+                current.shape.kind === 'points'
+                  ? {
+                      ...current,
+                      shape: {
+                        ...current.shape,
+                        releaseIndex: Number(event.currentTarget.value),
+                      },
+                    }
+                  : current,
+              )
+            }
+          >
+            <For each={shape().points}>
+              {(_point, index) => (
+                <option value={String(index())} selected={index() === shape().releaseIndex}>
+                  {index()}
+                </option>
+              )}
+            </For>
+          </select>
         </div>
       )}
     </Show>
